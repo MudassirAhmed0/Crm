0d479c2bb0ebb12bbd05e4965cc383423d6c1aa9
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-04-11 08:32:58 +0500

    origin

--- src/components/pages/dashboard/PerformanceChart.js
+++ src/components/pages/dashboard/PerformanceChart.js
@@ -1,11 +1,10 @@
 import React from "react";
+import MyChart from "./MyChart";
 
 const PerformanceChart = () => {
   return (
-    <div className="w-[52.96875vw] h-full border20 bg-[#fff] flex justify-center items-center">
-      <span className="text-center w-full text-red-600 text-[5vw]">
-        Performance
-      </span>
+    <div className="w-[52.96875vw] h-full border20 bg-[#fff] flex justify-center items-center relative">
+      <MyChart />
     </div>
   );
 };
